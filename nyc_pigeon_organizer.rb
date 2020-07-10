09606100a0815581e41ff9192e3754998f6f6e91
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-10 04:54:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   final = data.each_with_object({}) do |(key, value), final_array|
     value.each do |innerKey, names|
       names.each do |name|
@@ -10,6 +11,12 @@ def nyc_pigeon_organizer(data)
           !final_array[name][key] = []
         end
         final_array[name][key].push(innerKey.to_s)
+=======
+  data.each_with_object({}) do |(colorGenderLives_Hashes, insideColorGenderLives), final_array|
+    insideColorGenderLives.each do |names|
+      names.each do |key, value|
+        binding.pry
+>>>>>>> 6bdaafadbd1afcd2092b50451b1cdc3bca382048
       end
     end
   end
